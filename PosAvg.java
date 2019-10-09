6e8da5396578c367b81dc487087521776d61abb6
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-09 11:01:26 -0500

    PosAvg: Constructor and readFile

--- PosAvg.java
+++ PosAvg.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
 
 public class PosAvg {
-
+	
+	// Variables and arrays.
+	private String str;
+	private String temp;
+	ArrayList<String> file = new ArrayList<String>();
+	
+	// Default.
+	public PosAvg() {
+		
+	}
+	
+	public PosAvg(String str) {
+		// TODO
+	}
+	
+	// Reads in MesoNet.txt.
+	public void readFile(String fileName) {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
