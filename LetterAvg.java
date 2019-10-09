8667089af7fb4437212c79e23eda5ba69554a6e0
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-09 12:19:14 -0500

    LetterAvg: toString Constructor

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,6 +1,18 @@
 
 public class LetterAvg {
 
+	public String toString() {
+		String mesoOutput = "\nThey are:\n";
+		for(int i = 0; i < avgLetterStations.size(); ++i) {
+			if (i < (avgLetterStations.size()) -1) {
+				mesoOutput += avgLetterStations.get(i) + "\n";
+			} else {
+				mesoOuput += avgLetterStations.get(i);
+			}
+		}
+		return mesoOutput;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
