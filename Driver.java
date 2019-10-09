fe36969b8eb07e2e586f172ddbf672b20569a7cb
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-09 12:43:52 -0500

    Driver and LetterAvg Constructors (LetterAvg and numberOfStationWithLetterAvg())

--- Driver.java
+++ Driver.java
@@ -25,7 +25,9 @@ public class Driver
 		System.out.print("\n");
 		
 		LetterAvg letterAvg = new LetterAvg(mesoInherit.letterAverage());
-		System.out.println("Total number of stations starting with letter '" + mesoInherit.letterAverage() + "' is " + letterAvg.numberOfStationWithLetterAvg() + ".");
+		System.out.println("Total number of stations starting with letter '" 
+		+ mesoInherit.letterAverage() + "' is " 
+				+ letterAvg.numberOfStationWithLetterAvg() + ".");
 		
 		System.out.print(letterAvg);
 	}
--- LetterAvg.java
+++ LetterAvg.java
@@ -1,6 +1,10 @@
 
 public class LetterAvg {
 
+	public LetterAvg(char letterAverage) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public String toString() {
 		String mesoOutput = "\nThey are:\n";
 		for(int i = 0; i < avgLetterStations.size(); ++i) {
@@ -16,6 +20,11 @@ public class LetterAvg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public String numberOfStationWithLetterAvg() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
